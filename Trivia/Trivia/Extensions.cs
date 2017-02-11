fa0b7b23b88a9fc7501d84cc9c3a3f418162643e
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 15:01:00 +0200

    Add extension method to extract a question
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/Trivia/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    public static class Extensions
+    {
+        public static Question ExtractQuestion(this List<Question> questions, Question.Categories category)
+        {
+            foreach(Question q in questions)
+            {
+                if(q.category == category)
+                {
+                    questions.Remove(q);
+                    return q;
+                }
+            }
+
+            throw new QuestionNotFound("Question not Found");
+        }
+    }
+}
