ebe18bb8576d6e3141f2cb9eb68adb45b66e548b
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 10:51:03 +0200

    Add thest to check if empty player is created
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/TriviaTests/PlayerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivia;
+namespace TriviaTests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void EmptyPlayerIsCreated()
+        {
+            Player player = new Player("TestName");
+            Assert.AreEqual("TestName",player.Name);
+            Assert.AreEqual(0,player.Places);
+            Assert.AreEqual(0,player.Purses);
+            Assert.IsFalse(player.IsPenaltyBox);
+        }
+    }
+}
