99f3199452541da39a972bf916a8eb3af33d36ec
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 15:32:07 +0200

    Add test method for ExtractQuestion
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/TriviaTests/ExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivia;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriviaTests
+{
+    [TestClass]
+    public class ExtensionTests
+    {
+        [TestMethod]
+        public void ExtractQuestionRemoveQuestion()
+        {
+            List<Question> questions = new List<Question>();
+            for(int i = 0;i < 50;i++)
+            {
+                questions.Add(new Question(Categories.Pop,i));
+                questions.Add(new Question(Categories.Science,i));
+                questions.Add(new Question(Categories.Sport,i));
+                questions.Add(new Question(Categories.Rock,i));
+            }
+
+            questions.ExtractQuestion(Categories.Pop);
+            Assert.AreEqual(49,questions.Count(s=>s.category==Categories.Pop));
+            Assert.AreEqual(50,questions.Count(s => s.category == Categories.Science));
+            Assert.AreEqual(50,questions.Count(s => s.category == Categories.Sport));
+            Assert.AreEqual(50,questions.Count(s => s.category == Categories.Rock));
+
+        }
+    }
+}
