8e618de82d5d73e43f2809534b18f7abaa120c83
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 10:43:14 +0200

    Add Player class
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/Trivia/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+        string Name { get; }
+        int Places { get; set; }
+        int purses { get; set; }
+        bool IsPenaltyBox { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Places = 0;
+            purses = 0;
+            IsPenaltyBox = false;
+        }
+    }
+}
