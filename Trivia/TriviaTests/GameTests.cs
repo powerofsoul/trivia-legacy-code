5c1a8018e6dc5fb2ad8e90053f5d2016ef318e6f
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 10:17:34 +0200

    Add method to test if game is playable
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- Trivia/TriviaTests/GameTests.cs
+++ Trivia/TriviaTests/GameTests.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UglyTrivia;
 
 namespace TriviaTests
 {
     [TestClass]
     public class GameTests
     {
-        [TestMethod]
-        public void GameHasEnded()
-        {
+        private Game _game;
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void GameIsPlayable()
+        {
+            _game.add("Test");
+            _game.add("Test2");
+            Assert.IsTrue(_game.isPlayable());
         }
     }
 }
