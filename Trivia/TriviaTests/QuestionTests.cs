242a21c7ee2e6532ef89a00ba884b75a0f3f8795
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 14:36:10 +0200

    Add tests for Question class
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/TriviaTests/QuestionTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivia;
+
+namespace TriviaTests
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestMethod]
+        public void QuestionIsCreatedSuccesfuly()
+        {
+            Question question = new Question(Question.Categories.Pop,10);
+            Assert.AreEqual(Question.Categories.Pop,question.category);
+            Assert.AreEqual(10,question.Id);
+        }
+    }
+}
