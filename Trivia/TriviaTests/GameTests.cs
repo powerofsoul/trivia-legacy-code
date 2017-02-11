a0a94cbae6eb57b3d3a98585a16fa566affd9329
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 09:54:43 +0200

    Add tests project
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/TriviaTests/GameTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriviaTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void GameHasEnded()
+        {
+
+        }
+    }
+}
