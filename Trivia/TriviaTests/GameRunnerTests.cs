887de8c303b26658c277fe602f796ad9812fa66f
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-11 10:34:42 +0200

    Add test to check if game has ended
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ Trivia/TriviaTests/GameRunnerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Trivia;
+namespace TriviaTests
+{
+    [TestClass]
+    public class GameRunnerTests
+    {
+        [TestMethod, Timeout(2000)]
+        public void TheGameIsEnding()
+        {
+            GameRunner.Main(new string[0]);
+            Assert.AreEqual(0,0);
+        }
+    }
+}
