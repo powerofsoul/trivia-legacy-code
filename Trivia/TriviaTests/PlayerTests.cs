c169d488d83347202ebad26b1cd7a004bed37663
Florin Munteanu <florinmunteanu96@gmail.com>
2017-02-12 11:54:30 +0200

    Add test initialize for playertests
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- Trivia/TriviaTests/PlayerTests.cs
+++ Trivia/TriviaTests/PlayerTests.cs
@@ -6,10 +6,18 @@ namespace TriviaTests
     [TestClass]
     public class PlayerTests
     {
+        private Player player;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            player = new Player("TestPlayer");
+        }
+
         [TestMethod]
         public void EmptyPlayerIsCreated()
         {
-            Player player = new Player("TestName");
+            player = new Player("TestName");
             Assert.AreEqual("TestName",player.Name);
             Assert.AreEqual(0,player.Places);
             Assert.AreEqual(0,player.Purses);
